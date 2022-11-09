048d4fb795e7970e4453d37cf920b992b0e825de
WaitSpring <me@waitspring.com>
2022-11-09 21:00:17 +0800

    add toolsredirect
    Signed-off-by: WaitSpring <me@waitspring.com>

---
+++ src/ToolsRedirect-opt-bolds/ToolsRedirect-opt-bolds.js
@@ -0,0 +1,13 @@
+/**
+ * SPDX-License-Identifier: CC-BY-SA-3.0
+ * _addText: '{{Gadget Header|license=CC-BY-SA-3.0}}'
+ *
+ * @url https://www.qiuwenbaike.cn/wiki/MediaWiki:Gadget-ToolsRedirect-opt-bolds.js
+ * @source https://zh.wikipedia.org/wiki/MediaWiki:Gadget-ToolsRedirect-opt-bolds.js
+ * @license <https://creativecommons.org/licenses/by-sa/3.0>
+ */
+'use strict';
+
+mw.loader.using([ 'ext.gadget.ToolsRedirect' ], function () {
+	mw.toolsRedirect.findRedirectBySelector('div#mw-content-text p > b');
+});
