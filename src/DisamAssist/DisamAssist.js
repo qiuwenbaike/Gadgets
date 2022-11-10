f745ac58183c0e6ed108d312466abf97eb23574a
WaitSpring <me@waitspring.com>
2022-11-10 20:09:01 +0800

    update eslint settings
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/DisamAssist/DisamAssist.js
+++ src/DisamAssist/DisamAssist.js
@@ -1,3 +1,5 @@
+'use strict';
+
 // <nowiki>
 /**
  * DisamAssist
@@ -9,112 +11,82 @@
 window.DisamAssist = jQuery.extend(true, {
 	cfg: {
 		/*
-		 * Categories where disambiguation pages are added (usually by a template like {{Disambiguation}}
-		 */
+     * Categories where disambiguation pages are added (usually by a template like {{Disambiguation}}
+     */
 		disamCategories: [ '全部消歧義頁面' ],
-
 		/*
-		 * "Canonical names" of the templates that may appear after ambiguous links
-		 * and which should be removed when fixing those links
-		 */
-		disamLinkTemplates: [
-			'Disambiguation needed',
-			'Ambiguous link',
-			'Amblink',
-			'Dab needed',
-			'Disamb-link',
-			'Disambig needed',
-			'Disambiguate',
-			'Dn',
-			'Needdab'
-		],
-
+     * "Canonical names" of the templates that may appear after ambiguous links
+     * and which should be removed when fixing those links
+     */
+		disamLinkTemplates: [ 'Disambiguation needed', 'Ambiguous link', 'Amblink', 'Dab needed', 'Disamb-link', 'Disambig needed', 'Disambiguate', 'Dn', 'Needdab' ],
 		/*
-		 * "Canonical names" of the templates that designate intentional links to
-		 * disambiguation pages
-		 */
-		disamLinkIgnoreTemplates: [
-			'R from ambiguous page',
-			'R to disambiguation page',
-			'R from incomplete disambiguation'
-		],
-
+     * "Canonical names" of the templates that designate intentional links to
+     * disambiguation pages
+     */
+		disamLinkIgnoreTemplates: [ 'R from ambiguous page', 'R to disambiguation page', 'R from incomplete disambiguation' ],
 		/*
-		 * Format string for "Foo (disambiguation)"-style pages
-		 */
+     * Format string for "Foo (disambiguation)"-style pages
+     */
 		disamFormat: '$1 (消歧义)',
-
 		/*
-		 * Regular expression matching the titles of disambiguation pages (when they are different from
-		 * the titles of the primary topics)
-		 */
+     * Regular expression matching the titles of disambiguation pages (when they are different from
+     * the titles of the primary topics)
+     */
 		disamRegExp: '^(.*) \\((消歧义|消歧義)\\)$',
-
 		/*
-		 * Text that will be inserted after the link if the user requests help. If the value is null,
-		 * the option to request help won't be offered
-		 */
+     * Text that will be inserted after the link if the user requests help. If the value is null,
+     * the option to request help won't be offered
+     */
 		disamNeededText: '{{dn|date=February 2022}}',
-
 		/*
-		 * Content of the "Foo (disambiguation)" pages that will be created automatically when using
-		 * DisamAssist from a "Foo" page
-		 */
+     * Content of the "Foo (disambiguation)" pages that will be created automatically when using
+     * DisamAssist from a "Foo" page
+     */
 		redirectToDisam: '#REDIRECT [[$1]] {{R to disambiguation page}}',
-
 		/*
-		 * Whether intentional links to disambiguation pages can be explicitly marked by adding " (disambiguation)"
-		 */
+     * Whether intentional links to disambiguation pages can be explicitly marked by adding " (disambiguation)"
+     */
 		intentionalLinkOption: false,
-
 		/*
-		 * Namespaces that will be searched for incoming links to the disambiguation page (pages in other
-		 * namespaces will be ignored)
-		 */
+     * Namespaces that will be searched for incoming links to the disambiguation page (pages in other
+     * namespaces will be ignored)
+     */
 		targetNamespaces: [ 6, 10, 14, 100, 108, 0 ],
-
 		/*
-		 * Number of backlinks that will be downloaded at once
-		 * When using blredirect, the maximum limit is supposedly halved
-		 * (see http://www.mediawiki.org/wiki/API:Backlinks)
-		 */
+     * Number of backlinks that will be downloaded at once
+     * When using blredirect, the maximum limit is supposedly halved
+     * (see http://www.mediawiki.org/wiki/API:Backlinks)
+     */
 		backlinkLimit: 250,
-
 		/*
-		 * Number of titles we can query for at once
-		 */
+     * Number of titles we can query for at once
+     */
 		queryTitleLimit: 50,
-
 		/*
-		 * Number of characters before and after the incoming link that will be displayed
-		 */
+     * Number of characters before and after the incoming link that will be displayed
+     */
 		radius: 300,
-
 		/*
-		 * Height of the context box, in lines
-		 */
+     * Height of the context box, in lines
+     */
 		numContextLines: 4,
-
 		/*
-		 * Number of pages that will be stored before saving, so that changes to them can be
-		 * undone if need be
-		 */
+     * Number of pages that will be stored before saving, so that changes to them can be
+     * undone if need be
+     */
 		historySize: 2,
-
 		/*
-		 * Minimum time in seconds since the last change was saved before a new edit can be made. A
-		 * negative value or 0 disables the cooldown. Users with the "bot" right won't be affected by
-		 * the cooldown
-		 */
+     * Minimum time in seconds since the last change was saved before a new edit can be made. A
+     * negative value or 0 disables the cooldown. Users with the "bot" right won't be affected by
+     * the cooldown
+     */
 		editCooldown: 0,
-
 		/*
-		 * Specify how the watchlist is affected by DisamAssist edits. Possible values: "watch", "unwatch",
-		 * "preferences", "nochange"
-		 */
+     * Specify how the watchlist is affected by DisamAssist edits. Possible values: "watch", "unwatch",
+     * "preferences", "nochange"
+     */
 		watch: 'nochange'
 	},
-
 	txt: {
 		start: '为链接消歧义',
 		startMain: '为链至主条目的链接消歧义',
@@ -158,6 +130,5 @@ window.DisamAssist = jQuery.extend(true, {
 		redirectSummary: '使用[[MediaWiki:Gadget-DisamAssist.js|DisamAssist]]创建目标为[[$1]]的重定向。'
 	}
 }, window.DisamAssist || {});
-
 mw.loader.load('/index.php?title=MediaWiki:Gadget-DisamAssist-core.js&action=raw&ctype=text/javascript');
 // </nowiki>
