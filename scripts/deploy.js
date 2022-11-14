66880a1d2903a16b28e9ecfac20820770af04c76
WaitSpring <me@waitspring.com>
2022-11-14 23:40:47 +0800

    add MIT LICENSE text
    Signed-off-by: WaitSpring <me@waitspring.com>

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,31 @@
 /**
  * @source https://github.com/wikimedia-gadgets/xfdcloser/blob/master/bin/deploy.js
- * @license MIT
+ * @license MIT + CC-BY-SA-3.0 + CC-BY-4.0
  */
-// Adapted from https://github.com/wikimedia-gadgets/xfdcloser/blob/master/bin/deploy.js
-// (MIT License)
+// This software is published under the following licenses. You may select the license of your choice.
+// - Note: Files published on Wikipedia, including previous versions of XFDcloser, are also available under
+// Creative Commons Attribution-ShareAlike 3.0 Unported License (CC BY-SA 3.0)
+// https://creativecommons.org/licenses/by-sa/3.0/ and GNU Free Documentation License (GFDL)
+// http://www.gnu.org/copyleft/fdl.html
+// ---------------------------------------------------------------------------------------------------
+// MIT License
+// Copyright (c) 2020 Evad37
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+// and associated documentation files (the "Software"), to deal in the Software without
+// restriction, including without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+// the Software is furnished to do so, subject to the following conditions:
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+// ---------------------------------------------------------------------------------------------------
+// Creative Commons Attribution 4.0 International Public License
+// For a human-readable summary, see https://creativecommons.org/licenses/by/4.0/
+
 /**
  * This script is used to deploy files to the wiki.
  * You must have interface-admin rights to deploy as gadget.
