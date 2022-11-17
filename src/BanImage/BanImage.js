8d846fb06ddae5d1594a1b6fe496896f923fa240
WaitSpring <me@waitspring.com>
2022-11-17 17:27:11 +0800

    revert: add window.onload
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/BanImage/BanImage.js
+++ src/BanImage/BanImage.js
@@ -12,30 +12,28 @@
 'use strict';
 
 mw.loader.using([ 'ext.gadget.Wikiplus' ], (function (mw) {
-	window.onload = function onload() {
-		window.banPage = function banPage(targetName, currentName) {
-			window.Wikiplus.kotori.redirectTo(targetName, currentName, {
-				success: function success() {
-					Wikiplus.notice.create.success('创建重定向成功！');
-					location.href = mw.config.get('wgArticlePath').replace(/\$1/gi, currentName);
-				},
-				fail: function fail(d) {
-					Wikiplus.notice.create.error('错误：' + d.message);
-				}
-			});
-		};
-		window.banImage = window.banPage('File:Banned Images.svg', mw.config.get('wgPageName'));
-		window.banArticle = window.banPage('Qiuwen:首页', mw.config.get('wgPageName'));
-		window.banTemplate = window.banPage('Template:Void', mw.config.get('wgPageName'));
-
-		if (mw.config.get('wgNamespaceNumber') === 0) {
-			mw.util.addPortletLink('p-cactions', 'javascript:window.banArticle();', '禁用此页面');
-		}
-		if (mw.config.get('wgNamespaceNumber') === 6) {
-			mw.util.addPortletLink('p-cactions', 'javascript:window.banImage();', '禁用此图片');
-		}
-		if (mw.config.get('wgNamespaceNumber') === 10) {
-			mw.util.addPortletLink('p-cactions', 'javascript:window.banTemplate();', '禁用此模板');
-		}
+	window.banPage = function banPage(targetName, currentName) {
+		window.Wikiplus.kotori.redirectTo(targetName, currentName, {
+			success: function success() {
+				Wikiplus.notice.create.success('创建重定向成功！');
+				location.href = mw.config.get('wgArticlePath').replace(/\$1/gi, currentName);
+			},
+			fail: function fail(d) {
+				Wikiplus.notice.create.error('错误：' + d.message);
+			}
+		});
 	};
+	window.banImage = window.banPage('File:Banned Images.svg', mw.config.get('wgPageName'));
+	window.banArticle = window.banPage('Qiuwen:首页', mw.config.get('wgPageName'));
+	window.banTemplate = window.banPage('Template:Void', mw.config.get('wgPageName'));
+
+	if (mw.config.get('wgNamespaceNumber') === 0) {
+		mw.util.addPortletLink('p-cactions', 'javascript:window.banArticle();', '禁用此页面');
+	}
+	if (mw.config.get('wgNamespaceNumber') === 6) {
+		mw.util.addPortletLink('p-cactions', 'javascript:window.banImage();', '禁用此图片');
+	}
+	if (mw.config.get('wgNamespaceNumber') === 10) {
+		mw.util.addPortletLink('p-cactions', 'javascript:window.banTemplate();', '禁用此模板');
+	}
 }(mediaWiki)));
