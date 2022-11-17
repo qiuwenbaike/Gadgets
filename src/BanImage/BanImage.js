6164bcf75bdf4fd9f178da73914decc842029128
WaitSpring <me@waitspring.com>
2022-11-17 17:28:36 +0800

    reconstruct
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/BanImage/BanImage.js
+++ src/BanImage/BanImage.js
@@ -11,22 +11,22 @@
 /* eslint-disable no-script-url */
 'use strict';
 
-mw.loader.using([ 'ext.gadget.Wikiplus' ], (function (mw) {
-	window.banPage = function banPage(targetName, currentName) {
-		window.Wikiplus.kotori.redirectTo(targetName, currentName, {
-			success: function success() {
-				Wikiplus.notice.create.success('创建重定向成功！');
-				location.href = mw.config.get('wgArticlePath').replace(/\$1/gi, currentName);
-			},
-			fail: function fail(d) {
-				Wikiplus.notice.create.error('错误：' + d.message);
-			}
-		});
-	};
-	window.banImage = window.banPage('File:Banned Images.svg', mw.config.get('wgPageName'));
-	window.banArticle = window.banPage('Qiuwen:首页', mw.config.get('wgPageName'));
-	window.banTemplate = window.banPage('Template:Void', mw.config.get('wgPageName'));
+window.banPage = function banPage(targetName, currentName) {
+	window.Wikiplus.kotori.redirectTo(targetName, currentName, {
+		success: function success() {
+			Wikiplus.notice.create.success('创建重定向成功！');
+			location.href = mw.config.get('wgArticlePath').replace(/\$1/gi, currentName);
+		},
+		fail: function fail(d) {
+			Wikiplus.notice.create.error('错误：' + d.message);
+		}
+	});
+};
+window.banImage = window.banPage('File:Banned Images.svg', mw.config.get('wgPageName'));
+window.banArticle = window.banPage('Qiuwen:首页', mw.config.get('wgPageName'));
+window.banTemplate = window.banPage('Template:Void', mw.config.get('wgPageName'));
 
+$(function () {
 	if (mw.config.get('wgNamespaceNumber') === 0) {
 		mw.util.addPortletLink('p-cactions', 'javascript:window.banArticle();', '禁用此页面');
 	}
@@ -36,4 +36,4 @@ mw.loader.using([ 'ext.gadget.Wikiplus' ], (function (mw) {
 	if (mw.config.get('wgNamespaceNumber') === 10) {
 		mw.util.addPortletLink('p-cactions', 'javascript:window.banTemplate();', '禁用此模板');
 	}
-}(mediaWiki)));
+});
