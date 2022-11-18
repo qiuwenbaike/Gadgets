9d6190b18e78368f7a2e22dca8cc89e698913631
WaitSpring <me@waitspring.com>
2022-11-18 22:52:57 +0800

    fix bug: <th> displays on the <tfoot>
    Signed-off-by: WaitSpring <me@waitspring.com>

--- src/Definitions/Definitions.js
+++ src/Definitions/Definitions.js
@@ -223,9 +223,15 @@ if (mw.config.get('wgPageName') === 'MediaWiki:Gadgets-definition' && mw.config.
 				};
 			// eslint-disable-next-line no-jquery/no-sizzle
 			$('h2:has( .mw-editsection )').next().children('ul').addBack('ul').replaceWith(function () {
-				return $('<table>').attr('class', 'wikitable sortable defTable').html($('<tbody>').html($('<tr>').html([ '名称', '类型', '默认', 'Peers', '依赖项', '权限', '范围', '皮肤', '隐藏', '链接' ].map(function (ele) {
-					return $('<th>').text(ele);
-				}))).add($(this).children().map(insertRow))).tablesorter().on('dblclick', 'td', edit).on('click', '.oo-ui-indicator-clear', deleteRow);
+				return $('<table>').attr('class', 'wikitable sortable defTable').html(
+					$('<tbody>').html(
+						$('<tr>').html(
+							[ '名称', '类型', '默认', 'Peers', '依赖项', '权限', '范围', '皮肤', '隐藏', '链接' ].map(function (ele) {
+								return $('<th>').text(ele);
+							})
+						).add($(this).children().map(insertRow))
+					)
+				).tablesorter().on('dblclick', 'td', edit).on('click', '.oo-ui-indicator-clear', deleteRow);
 			});
 			btns[0].on('click', save);
 			btns[1].$element.on('click', function () {
